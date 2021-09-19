b9db9bcee4a4295414e62bd34e06d2cdb122f5ed
Low0409 <kitagawa0409@icloud.com>
2021-09-19 10:57:09 +0900

    修正4

--- pages/Components/Header.js
+++ pages/Components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import Styles from '../../styles/Home.module.css'
 const Header = () => {
     return (
         <>
           <div className="Header">
-            <h1>Header</h1>
+          <span>
+                <div className={Styles.hamburgerMenu}>
+                    <input type="checkbox" className={Styles.menuBtnCheck} />
+                    <label htmlFor="menuBtnCheck" className={Styles.menuBtn}><span /></label>
+                    <div className={Styles.MenuContent}>
+                        <ul>
+                            <li><a href="#">メニューアイテム１</a></li>
+                            <li><a href="#">メニューアイテム２</a></li>
+                            <li><a href="#">メニューアイテム３</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </span>
+            <h1 className="HeaderText">Header</h1>
           </div>
         </>
     )
